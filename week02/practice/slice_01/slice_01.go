3316a897c760f75a2e8314aa592194ca4890fe76
fukun <byfukun@foxmail.com>
2025-03-27 16:30:36 +0800

    ADD slice FILES

---
+++ week02/practice/slice_01/slice_01.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := [10]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	slice := arr[2:7]
+	slice = append(slice, 100)
+	sum := 0
+	for _, value := range slice {
+		sum += value
+	}
+	fmt.Println(slice)
+	fmt.Println(sum)
+}
