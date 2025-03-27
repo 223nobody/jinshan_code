4b3bb27285e744003e092b4c5cc8ac404ab5b21a
fukun <byfukun@foxmail.com>
2025-03-27 20:08:59 +0800

    ADD FILES

---
+++ week02/practice/struct_01/struct_01.go
@@ -0,0 +1,39 @@
+﻿package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	Name  string
+	Age   int
+	Email string
+}
+
+func main() {
+	p := NewPerson("付坤", 20, "2141024586@qq.com")
+	p.PrintPerson()
+
+	jsonData, err := json.MarshalIndent(p, "", "  ")
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println(string(jsonData))
+}
+
+func NewPerson(name string, age int, email string) Person {
+	p := Person{
+		Name:  name,
+		Age:   age,
+		Email: email,
+	}
+	return p
+}
+
+func (p Person) PrintPerson() {
+	fmt.Println("姓名是：", p.Name)
+	fmt.Println("年龄是：", p.Age)
+	fmt.Println("邮箱是：", p.Email)
+}
---
+++ week02/practice/struct_02/struct_02.go
@@ -0,0 +1,27 @@
+﻿package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type Person struct {
+	Name  string `json:"name"`
+	Age   int    `json:"age"`
+	Email string `json:"email"`
+}
+
+func main() {
+
+	jsonData := ` {"name":"Jane Smith","age":25,"email":"janesmith@example.com"}`
+
+	var p Person
+	err := json.Unmarshal([]byte(jsonData), &p)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	fmt.Println("Name:", p.Name)
+	fmt.Println("Age:", p.Age)
+	fmt.Println("Email:", p.Email)
+}
