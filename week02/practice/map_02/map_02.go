e002b2206d46d75aa23297f763107893133292ed
fukun <byfukun@foxmail.com>
2025-03-27 17:19:05 +0800

    map_02.go

---
+++ week02/practice/map_02/map_02.go
@@ -0,0 +1,27 @@
+﻿package main
+
+import (
+	"fmt"
+)
+
+func countCharacters(s string) rune {
+	counter := make(map[rune]int)
+	var a rune
+	n := 0
+	for _, char := range s {
+		counter[char]++
+	}
+	for key, value := range counter {
+		if value >= n {
+			n = value
+			a = key
+		}
+	}
+	return a
+}
+func main() {
+	var str string
+	fmt.Scanln(&str)
+	char := countCharacters(str)
+	fmt.Printf("出现最多的字符为： %c", char)
+}
