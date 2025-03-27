f5d35417ba0488c08bd7c2ebd1b2e126e2331c50
fukun <byfukun@foxmail.com>
2025-03-27 17:18:43 +0800

    map_01.go

---
+++ week02/practice/map_01/map_01.go
@@ -0,0 +1,18 @@
+﻿package main
+
+import "fmt"
+
+func main() {
+	scores := map[string]int{
+		"小明": 60,
+		"小王": 70,
+		"张三": 95,
+		"李四": 98,
+		"王五": 100,
+		"张伟": 88,
+	}
+	for key, value := range scores {
+		fmt.Printf("%s的成绩是 %d\n", key, value)
+
+	}
+}
