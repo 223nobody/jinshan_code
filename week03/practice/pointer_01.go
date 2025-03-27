9fcf9950bfa552b10a7b20cf1679b8b1a5afe925
fukun <byfukun@foxmail.com>
2025-03-27 14:42:09 +0800

    ADD pointer_01.go FILE

---
+++ week03/practice/pointer_01.go
@@ -0,0 +1,19 @@
+﻿package main
+
+import "fmt"
+
+func Swap(a, b *int) {
+	p := 0
+	q := &p
+	*q = *a
+	*a = *b
+	*b = *q
+}
+
+func main() {
+	num1 := 5
+	num2 := 10
+	fmt.Printf("交换前：num1 = %d , num2 = %d\n", num1, num2)
+	Swap(&num1, &num2)
+	fmt.Printf("交换后：num1 = %d , num2 = %d\n", num1, num2)
+}
