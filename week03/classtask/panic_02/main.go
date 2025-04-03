29627b622c8f7b4f4fd1a767aa8b9a1e0920f272
fukun <byfukun@foxmail.com>
2025-04-03 16:42:02 +0800

    ADD FILES

---
+++ week03/classtask/panic_02/main.go
@@ -0,0 +1,25 @@
+﻿package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n int
+	fmt.Scanln(&n)
+	num := []int{1, 2, 3, 4, 5}
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Printf("Recovered from: %v\n", r)
+		}
+	}()
+	fmt.Println(accessArray(n, num))
+
+}
+
+func accessArray(n int, arr []int) int {
+	if n >= len(arr) {
+		panic("数组下标越界")
+	}
+	return arr[n]
+}
